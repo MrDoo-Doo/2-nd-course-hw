4702a9cdb61abaa57fcbebc41f7d7602615a8874
Алексей <efremovalexei02@gmail.com>
2025-04-06 13:35:00 +0500

    Tasks (JS) + add Mini-game section

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*
 // Задание №1
 let a = 10;
 alert(a);
@@ -53,3 +54,101 @@ number1 /= 2;
 alert(number1);
 number1 -= number;
 alert("Ответ: 5");
+*/
+// Задание №1
+
+let password = 'пароль';
+let check = prompt('Введите пароль:');
+(password === check) ? alert('Пароль введен верно') : alert('Пароль введен неправильно');
+// Задание №2
+let c = prompt('Введите число большее 0 и меньшее 10:');
+((c > 0) && (c < 10)) ? alert('Верно') : alert('Неверно');
+// Задание №3
+let d = prompt('Напишите одно любое число:');
+let e = prompt('Напишите другое любое число:');
+((d > 100) || (e > 100)) ? alert('Верно') : alert('Неверно');
+// Задание №4
+let a = '2';
+let b = '3';
+alert(Number(a) + Number(b));
+// Задание №5
+let monthNumber = Number(prompt('Введите номер месяца:'));
+switch (monthNumber) {
+    case 1:
+        alert('Это зима');
+        break;
+    case 2:
+        alert('Это зима');
+        break;
+    case 3:
+        alert('Это весна');
+        break;
+    case 4:
+        alert('Это весна');
+        break;
+    case 5:
+        alert('Это весна');
+        break;
+    case 6:
+        alert('Это лето');
+        break;
+    case 7:
+        alert('Это лето');
+        break;
+    case 8:
+        alert('Это лето');
+        break;
+    case 9:
+        alert('Это осень');
+        break;
+    case 10:
+        alert('Это осень');
+        break;
+    case 11:
+        alert('Это осень');
+        break;
+    case 12:
+        alert('Это зима');
+        break;
+    default:
+        alert('Нет такого месяца');
+}
+// Задание №1+
+let num = prompt('Пожалуйста, введите любое число:');
+let nn = isNaN(num);
+if (nn == false) {
+    (num % 2 == 0) ? alert('Это число чётное') : alert('Это число нечётное');
+}
+else alert('Это не число');
+// Задание №2+
+let clientOS = prompt('Какая ОС у клиента (0/1):');
+if (clientOS == 0) {
+    alert('Установите версию приложения для iOS по ссылке');
+}
+else if (clientOS == 1) {
+    alert('Установите версию приложения для Android по ссылке');
+}
+else {
+    alert('Ошибка');
+}
+// Задание №3+
+let clientYear = prompt('Какого года Ваш телефон:');
+if (clientOS == 0) {
+    if (clientYear < 2015) {
+        alert('Установите облегченную версию приложения для iOS по ссылке');
+    }
+    if (clientYear >= 2015) {
+        alert('Установите версию приложения для iOS по ссылке');
+    }
+}
+else if (clientOS == 1) {
+    if (clientYear < 2015) {
+        alert('Установите облегченную версию приложения для Android по ссылке');
+    }
+    if (clientYear >= 2015) {
+        alert('Установите версию приложения для Android по ссылке');
+    }
+}
+else {
+    alert('Ошибка');
+}
