412adb33e061101acb7541c56a20efba81359ccf
Алексей <efremovalexei02@gmail.com>
2025-04-09 00:34:15 +0500

    hw4: JS-tasks + mob ver. segment mini-game

--- main.js
+++ main.js
@@ -1,89 +1,58 @@
 // Задание №1
-let password = 'пароль';
-let check = prompt('Введите пароль:');
-if (password === check)
-    alert('Пароль введен верно');
-else
-    alert('Пароль введен неправильно');
+let a = 1;
+while (a <= 2) {
+    console.log('Привет');
+    a++;
+}
 // Задание №2
-let c = prompt('Введите число большее 0 и меньшее 10:');
-if ((c > 0) && (c < 10))
-    alert('Верно');
-else
-    alert('Неверно');
+let b = 1;
+while (b <= 5) {
+    console.log(b);
+    b++;
+}
 // Задание №3
-let d = prompt('Напишите одно любое число:');
-let e = prompt('Напишите другое любое число:');
-if ((d > 100) || (e > 100))
-    alert('Верно');
-else
-    alert('Неверно');
+let c = 7;
+while (c <= 22) {
+    console.log(c);
+    c++;
+}
 // Задание №4
-let a = '2';
-let b = '3';
-alert(Number(a) + Number(b));
+let obj = {
+    Коля: '200',
+    Вася: '300',
+    Петя: '400'
+}
+for (let key in obj) {
+    console.log(`${key}: - зарплата ${obj[key]} долларов`);
+}
 // Задание №5
-let monthNumber = Number(prompt('Введите номер месяца:'));
-switch (monthNumber) {
-    case 1:
-    case 2:
-    case 12:
-        alert('Это зима');
-        break;
-    case 3:
-    case 4:
-    case 5:
-        alert('Это весна');
-        break;
-    case 6:
-    case 7:
-    case 8:
-        alert('Это лето');
-        break;
-    case 9:
-    case 10:
-    case 11:
-        alert('Это осень');
-        break;
-    default:
-        alert('Нет такого месяца');
+let n = 1000;
+do {
+    n /= 2;
+} while (n >= 50)
+console.log(n);
+// Задание №6
+let friday = 2;
+do {
+    console.log(`Сегодня пятница, ${friday}-е число. Необходимо подготовить отчет.`);
+    friday += 7;
+} while (friday <= 31);
+// Задание №1-1
+let iterations = 0;
+let k = 100;
+while (k >= 0) {
+    k -= 7;
+    iterations++;
 }
-// Задание №1+
-let num = prompt('Пожалуйста, введите любое число:');
-let nn = isNaN(num);
-if (nn == false) {
-    (num % 2 == 0) ? alert('Это число чётное') : alert('Это число нечётное');
+// Задание №1-2
+
+// Задание №1-3
+let book = {
+    Название: 'Вино из одуванчиков',
+    Автор: 'Рэй Брэдбери',
+    "Год издания": 1957,
+    Жанр: 'Роман'
 }
-else alert('Это не число');
-// Задание №2+
-let clientOS = prompt('Какая ОС у клиента (0/1):');
-if (clientOS == 0) {
-    alert('Установите версию приложения для iOS по ссылке');
-}
-else if (clientOS == 1) {
-    alert('Установите версию приложения для Android по ссылке');
-}
-else {
-    alert('Ошибка');
-}
-// Задание №3+
-let clientYear = prompt('Какого года Ваш телефон:');
-if (clientOS == 0) {
-    if (clientYear < 2015) {
-        alert('Установите облегченную версию приложения для iOS по ссылке');
-    }
-    if (clientYear >= 2015) {
-        alert('Установите версию приложения для iOS по ссылке');
-    }
-}
-else if (clientOS == 1) {
-    if (clientYear < 2015) {
-        alert('Установите облегченную версию приложения для Android по ссылке');
-    }
-    if (clientYear >= 2015) {
-        alert('Установите версию приложения для Android по ссылке');
-    }
-}
-else {
-    alert('Ошибка');
+for (let page in book) {
+    console.log(`${page}: ${book[page]}`);
 }
