0bcec7b9fb4019f7d9d14b75324fdc1a53dc1e21
Алексей <efremovalexei02@gmail.com>
2025-05-01 16:04:06 +0500

    Complete Tasks JS in new HTML/JS file

--- tasks.js
+++ tasks.js
@@ -1,81 +1,48 @@
 // Задание №1
-const people = [
-    { name: 'Глеб', age: 29 },
-    { name: 'Анна', age: 17 },
-    { name: 'Олег', age: 7 },
-    { name: 'Оксана', age: 47 }
-];
-
-console.log(people.sort((a, b) => a.age - b.age));
-// Задание №2
-function isPositive(array) {
-    const arrReturn = [];
-    let j = 0;
-    for (i = 0; i < array.length; i++) {
-        if (array[i] > 0) {
-            arrReturn[j] = array[i];
-            j++;
-        }
+const textEl1 = document.querySelector('.text1');
+const buttonEl1 = document.querySelector('.button1');
+buttonEl1.addEventListener('click', () => {
+    textEl1.classList.toggle("hide");
+    if (buttonEl1.textContent === "Показать") {
+        buttonEl1.textContent = "Скрыть";
     }
-    return arrReturn;
-}
-
-function isMale(array) {
-    const arrReturn = [];
-    let i = 0;
-    array.forEach(el => {
-        if (el.gender == 'male') {
-            arrReturn[i] = el;
-            i++;
-        }
-    });
-    return arrReturn;
-}
-function filter(mas, callback) {
-    return callback(mas);
-}
-
-const people2 = [
-    { name: 'Глеб', gender: 'male' },
-    { name: 'Анна', gender: 'female' },
-    { name: 'Олег', gender: 'male' },
-    { name: 'Оксана', gender: 'female' }
-];
-
-console.log(filter([3, -4, 1, 9], isPositive));
-console.log(filter(people2, isMale));
-
+    else buttonEl1.textContent = "Показать";
+});
+// Задание №2
+const textEl2 = document.querySelector('.text2');
+const buttonEl2 = document.querySelector('.button2');
+buttonEl2.addEventListener('click', () => {
+    textEl2.classList.add("color");
+});
 // Задание №3
-let intervalTimer = setInterval(() => {
-    currentDate = new Date();
-    console.log(currentDate)
-}, 3000);
-
-setTimeout(() => {
-    clearInterval(intervalTimer);
-    console.log('30 секунд прошло');
-}, 30000);
-
+const textEl3 = document.querySelector('.text3');
+const buttonEl3 = document.querySelector('.button3');
+buttonEl3.addEventListener('click', () => {
+    textEl3.textContent = "Привет, мир!";
+});
 // Задание №4
-function delayForSecondTask4(callback) {
-    setTimeout(callback(), 1000);
-}
-
-delayForSecondTask4(function () {
-    console.log('Привет, Глеб!');
-})
-
+const textEl4 = document.querySelectorAll('.description1');
+textEl4.forEach(element => {
+    element.textContent = "Измененный текст";
+});
 // Задание №5
-function delayForSecondTask5(cb) {
-    setTimeout(() => {
-        console.log('Прошла одна секунда');
-        if (cb) { cb(); }
-    }, 1000)
-}
-
-function sayHi(name) {
-    console.log(`Привет, ${name}!`);
-}
-
-// Нужно изменить код ниже:
-delayForSecondTask5(() => sayHi('Глеб'));
+const textEl5 = document.querySelectorAll('.description2');
+textEl5.forEach(element => {
+    element.textContent = "Новый текст";
+});
+// Задание №6
+const containerEl6 = document.querySelector('.container6');
+const buttonEl6 = document.querySelector('.button6');
+buttonEl6.addEventListener('click', () => {
+    const newEl = document.createElement('h4');
+    newEl.textContent = 'Новый абзац';
+    containerEl6.appendChild(newEl);
+});
+// Задание №7
+const buttonEl7 = document.querySelector('.button7');
+buttonEl7.addEventListener('click', () => {
+    const textEl7 = document.querySelector('.description7');
+    if (textEl7) {
+        textEl7.remove();
+    }
+});
